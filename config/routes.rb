1696d9460ce7ada6ac95f4d2c957a354cd221319
Dicko Sow <s12.dsow@gmail.com>
2015-02-12 23:50:41 -0600

    created devise routes, views, user model and updated user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'visitors#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'application#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- db/migrate/20150213053518_devise_create_users.rb
+++ db/migrate/20150213053518_devise_create_users.rb
@@ -4,6 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :provider
+      t.string :uid
 
       ## Recoverable
       t.string   :reset_password_token
