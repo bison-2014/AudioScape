306992c9e9e2f196085a73115b3db8f6340d747f
Shea Strausman <sstrausman@gmail.com>
2015-02-14 13:17:28 -0600

    made the paths cleaner

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,7 +16,7 @@ class SongsController < ApplicationController
 
   def create
     song = Song.create(title: params[:song_name], artist: params[:song_artist], link: params[:song_id], playlist_id: params[:playlist_id])
-    redirect_to "/playlists/#{params[:playlist_id]}/songs/new"
+    redirect_to "/playlists/#{params[:playlist_id]}/"
 
   end
 
@@ -29,14 +29,15 @@ class SongsController < ApplicationController
   end
 
   def search
-    @songs = []
+    # @songs = []
 
     client = Grooveshark::Client.new({session: session[:groove_session]})
-    tracks = client.search_songs(params[:songs][:title])
+    @songs = client.search_songs(params[:songs][:title])
+    @artist_search_results = @songs.select { |song| song.artist.downcase == params[:songs][:artist].downcase}
 
-      tracks.each do |track|
-        @songs << track if track.artist.downcase == params[:songs][:artist] || track if track.artist.downcase == 'various artists'
-      end
+      # tracks.each do |track|
+      #   @songs << track if track.artist.downcase == params[:songs][:artist] || track if track.artist.downcase == 'various artists'
+      # end
 
   end
 
