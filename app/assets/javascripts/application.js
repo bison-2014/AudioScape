71b1a4cd4e52c8a289509c4a0533bbc4eb71101b
Michael Van Pelt & s12dsow <pair+mrvp194.s12dsow@devbootcamp.com>
2015-02-14 20:37:13 -0600

    songs now play like an actual playlist, also filtered current users around me function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,11 +26,12 @@ $(document).ready(function() {
   timeout           : 27000
   };
 
-  var geo_options = {
-  enableHighAccuracy: true,
-  maximumAge        : 30000,
-  timeout           : 27000
-  };
+  var currentdate = new Date();
+
+  console.log(currentdate)
+
+
+
 
   // Create a GeoFire index
   var geoFire = new GeoFire(firebaseRef);
@@ -49,21 +50,22 @@ $(document).ready(function() {
 
 
   function checkGeoFire(position) {
-    // geoFire.set(userId, [position.coords.latitude, position.coords.longitude])
-    geoFire.get(userId).then(function(location) {
-      if (location === null) {
-        geoFire.set(userId, [position.coords.latitude, position.coords.longitude]);
-        console.log('No key in database so we set the initial position')
-      }
-      else {
-        if (GeoFire.distance([position.coords.latitude, position.coords.longitude], location) > minUpdateUserDistance ) {
-          geoFire.set(userId, [position.coords.latitude, position.coords.longitude])
-          console.log('checked the key and we moved enough to update database')
-        }
-        }
-      }, function(error) {
-        console.log("Error: " + error);
-      });
+    geoFire.set(userId, [position.coords.latitude, position.coords.longitude])
+    firebaseRef.child(userId).update({'datetime': currentdate})
+    // geoFire.get(userId).then(function(location) {
+    //   if (location === null) {
+    //     geoFire.set(userId, {location:[position.coords.latitude, position.coords.longitude], dateTime: currentdate});
+    //     console.log('No key in database so we set the initial position')
+    //   }
+    //   else {
+    //     if (GeoFire.distance([position.coords.latitude, position.coords.longitude], location) > minUpdateUserDistance ) {
+    //       geoFire.set(userId, {location:[position.coords.latitude, position.coords.longitude], dateTime: currentdate})
+    //       console.log('checked the key and we moved enough to update database')
+    //     }
+    //     }
+    //   }, function(error) {
+    //     console.log("Error: " + error);
+    //   });
 
   }
 
@@ -79,6 +81,50 @@ getLocation()
 // });
   // var loc = 0;
 
+
+  var audio;
+  var playlist;
+  var tracks;
+  var current;
+
+  init();
+  function init(){
+      current = 0;
+      audio = $('audio');
+
+
+      playlist = $('#playlist');
+      tracks = playlist.find('span a');
+      firstTrackSrc = tracks.first().attr('href')
+      source = $('<source>').attr('type', 'audio/mpeg').attr('src', firstTrackSrc)
+      audio.append(source)
+      len = tracks.length - 1;
+      audio[0].volume = .10;
+      audio[0].play();
+      playlist.find('a').click(function(e){
+          e.preventDefault();
+          link = $(this);
+          current = link.parent().index();
+          run(link, audio[0]);
+      });
+      audio[0].addEventListener('ended',function(e){
+          current++;
+          if(current == len){
+              current = 0;
+              link = playlist.find('a')[0];
+          }else{
+              link = playlist.find('a')[current];
+          }
+          run($(link),audio[0]);
+      });
+  }
+  function run(link, player){
+          player.src = link.attr('href');
+          par = link.parent();
+          par.addClass('active').siblings().removeClass('active');
+          audio[0].load();
+          audio[0].play();
+  }
 
 
 
