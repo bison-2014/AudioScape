f697a217493446ee65da664cdbdb547b7b8c8cb8
Dicko Sow <s12.dsow@gmail.com>
2015-02-17 00:25:43 -0600

    added twitter authen and added back shared links

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,5 +1,5 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def spotify
+  def twitter
     @user = User.from_omniauth(request.env["omniauth.auth"])
     sign_in_and_redirect @user
   end
