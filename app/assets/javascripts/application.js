58bf6227b333c616cd8857761993239c39bc73c8
Raj Singh <raj@foreversingh.com>
2015-02-16 21:54:50 -0600

    set default volume to 80 percent

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,101 +16,105 @@
 //= require_tree .
 $(document).ready(function() {
 
-  var x = document.getElementById("demo");
-  var minUpdateUserDistance = 0.01 //km
-  var userId = $("div[style='display:none']").attr('id')
-  var firebaseRef = new Firebase("https://blinding-fire-43.firebaseio.com/");
-  var geo_options = {
-  enableHighAccuracy: true,
-  maximumAge        : 30000,
-  timeout           : 27000
-  };
+	var x = document.getElementById("demo");
+	var minUpdateUserDistance = 0.01 //km
+	var userId = $("div[style='display:none']").attr('id')
+	var firebaseRef = new Firebase("https://blinding-fire-43.firebaseio.com/");
+	var geo_options = {
+		enableHighAccuracy: true,
+		maximumAge: 30000,
+		timeout: 27000
+	};
 
-  var currentdate = new Date();
-
-
+	var currentdate = new Date();
 
 
-  // Create a GeoFire index
-  var geoFire = new GeoFire(firebaseRef);
-  console.log(userId)
-  function getLocation() {
-      if (navigator.geolocation) {
-          navigator.geolocation.watchPosition(checkGeoFire, null, geo_options);
-      } else {
-          x.innerHTML = "Geolocation is not supported by this browser.";
-      }
-  }
 
 
-  function checkGeoFire(position) {
-    // firebaseRef.push(userId)
+	// Create a GeoFire index
+	var geoFire = new GeoFire(firebaseRef);
+	console.log(userId)
 
-    geoFire.get(userId).then(function(location) {
-      if (location === null) {
-        geoFire.set(userId, [position.coords.latitude, position.coords.longitude]);
-        console.log('No key in database so we set the initial position')
-      }
-      else {
-        if (GeoFire.distance([position.coords.latitude, position.coords.longitude], location) > minUpdateUserDistance ) {
-          geoFire.set(userId, [position.coords.latitude, position.coords.longitude])
-          console.log('checked the key and we moved enough to update database')
-        }
-        }
-      }, function(error) {
-        console.log("Error: " + error);
-      });
-    firebaseRef.child(userId).update({'datetime': currentdate})
-  }
+	function getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.watchPosition(checkGeoFire, null, geo_options);
+		} else {
+			x.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	}
 
 
-  // geoFire.
+	function checkGeoFire(position) {
+		// firebaseRef.push(userId)
 
-getLocation()
+		geoFire.get(userId).then(function(location) {
+			if (location === null) {
+				geoFire.set(userId, [position.coords.latitude, position.coords.longitude]);
+				console.log('No key in database so we set the initial position')
+			} else {
+				if (GeoFire.distance([position.coords.latitude, position.coords.longitude], location) > minUpdateUserDistance) {
+					geoFire.set(userId, [position.coords.latitude, position.coords.longitude])
+					console.log('checked the key and we moved enough to update database')
+				}
+			}
+		}, function(error) {
+			console.log("Error: " + error);
+		});
+		firebaseRef.child(userId).update({
+			'datetime': currentdate
+		})
+	}
 
-  var audio;
-  var playlist;
-  var tracks;
-  var current;
 
-  init();
-  function init(){
-      current = 0;
-      audio = $('audio');
+	// geoFire.
+
+	getLocation()
+
+	var audio;
+	var playlist;
+	var tracks;
+	var current;
+
+	init();
+
+	function init() {
+		current = 0;
+		audio = $('audio');
 
 
-      playlist = $('#playlist');
-      tracks = playlist.find('span a');
-      firstTrackSrc = tracks.first().attr('href')
-      source = $('<source>').attr('type', 'audio/mpeg').attr('src', firstTrackSrc)
-      audio.append(source)
-      len = tracks.length - 1;
-      audio[0].volume = .10;
-      audio[0].play();
-      playlist.find('a').click(function(e){
-          e.preventDefault();
-          link = $(this);
-          current = link.parent().index();
-          run(link, audio[0]);
-      });
-      audio[0].addEventListener('ended',function(e){
-          current++;
-          if(current == len){
-              current = 0;
-              link = playlist.find('a')[0];
-          }else{
-              link = playlist.find('a')[current];
-          }
-          run($(link),audio[0]);
-      });
-  }
-  function run(link, player){
-          player.src = link.attr('href');
-          par = link.parent();
-          par.addClass('active').siblings().removeClass('active');
-          audio[0].load();
-          audio[0].play();
-  }
+		playlist = $('#playlist');
+		tracks = playlist.find('span a');
+		firstTrackSrc = tracks.first().attr('href')
+		source = $('<source>').attr('type', 'audio/mpeg').attr('src', firstTrackSrc)
+		audio.append(source)
+		len = tracks.length - 1;
+		audio[0].volume = .80;
+		audio[0].play();
+		playlist.find('a').click(function(e) {
+			e.preventDefault();
+			link = $(this);
+			current = link.parent().index();
+			run(link, audio[0]);
+		});
+		audio[0].addEventListener('ended', function(e) {
+			current++;
+			if (current == len) {
+				current = 0;
+				link = playlist.find('a')[0];
+			} else {
+				link = playlist.find('a')[current];
+			}
+			run($(link), audio[0]);
+		});
+	}
+
+	function run(link, player) {
+		player.src = link.attr('href');
+		par = link.parent();
+		par.addClass('active').siblings().removeClass('active');
+		audio[0].load();
+		audio[0].play();
+	}
 
 
 
