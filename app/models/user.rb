27767f995346abc4b889d2db600afddfc9086ceb
Dicko Sow <s12.dsow@gmail.com>
2015-02-18 01:15:05 -0600

    implementing tag a location feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:twitter]
   has_many :playlists
   has_many :songs, through: :playlists
+  has_many :taggings
+  has_many :locations, through: :taggings
+
 
   has_many :taggings
   has_many :locations, through: :taggings
