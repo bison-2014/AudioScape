4b56fc063048a00380ed9365a4b8ebb0594a1852
Dicko Sow <s12.dsow@gmail.com>
2015-02-18 12:36:33 -0600

    updated location and playlist logic

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,5 @@
 class Location < ActiveRecord::Base
-<<<<<<< HEAD
-
-	has_many :taggings
+	has_many :taggings, dependent: :delete_all
 	has_many :users, through: :taggings
 	has_many :playlists, through: :taggings
 end
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,7 +1,7 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
   has_many :songs
-  has_many :taggings
+  has_many :taggings, dependent: :delete_all
   has_many :locations, through: :taggings
 
   validates :title, presence: true
