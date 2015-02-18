650fef2e0d70fe7b8392182a00a7128315df4f78
Dicko Sow <s12.dsow@gmail.com>
2015-02-18 01:15:05 -0600

    implementing tag a location feature

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,8 @@
 class Location < ActiveRecord::Base
+<<<<<<< HEAD
 
 	has_many :taggings, dependent: :delete_all
 
 	has_many :users, through: :taggings
 	has_many :playlists, through: :taggings
-
 end
