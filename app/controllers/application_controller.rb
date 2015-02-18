c1bd5485b56e7b76832e5d2c0b4ce65c744274b7
s12dsow <s12.dsow@gmail.com>
2015-02-17 20:45:54 -0600

    updated logic for random playlist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,8 +30,6 @@ class ApplicationController < ActionController::Base
     current_users_around
   end
 
-
-
   helper_method :current_users_around
 
   private
@@ -40,16 +38,16 @@ class ApplicationController < ActionController::Base
   end
 
   def distance(lat1, long1, lat2, long2)
-    dtor = Math::PI/180
+    dtor = Math::PI / 180
     r = 6378.14
 
-    rlat1 = lat1 * dtor
+    rlat1  = lat1  * dtor
     rlong1 = long1 * dtor
-    rlat2 = lat2 * dtor
+    rlat2  = lat2  * dtor
     rlong2 = long2 * dtor
 
     dlon = rlong1 - rlong2
-    dlat = rlat1 - rlat2
+    dlat = rlat1  - rlat2
 
     a = power(Math::sin(dlat/2), 2) + Math::cos(rlat1) * Math::cos(rlat2) * power(Math::sin(dlon/2), 2)
     c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,24 @@
 module ApplicationHelper
 
   def groove_session
-    session[:groove_session]
+    Grooveshark::Client.new(session: session[:groove_session])
   end
 
+  def find_random_local_songs
+    random_songs = []
+    if current_users_around.empty?
+      result = "Sorry! No users around right now!"
+    else
+      current_users_around.each do |user|
+        unless user.playlists.empty?
+          @playlist = user.playlists.sample
+          unless @playlist.songs.empty?
+            song = @playlist.songs.sample
+            random_songs << song
+          end
+        end
+      end
+    end
+    return random_songs
+  end
 end
