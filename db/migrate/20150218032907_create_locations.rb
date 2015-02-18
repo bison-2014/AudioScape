eb66f89c393172f7715f588182c5469750622a3b
Dicko Sow <s12.dsow@gmail.com>
2015-02-18 11:21:03 -0600

    updated location logic

--- db/migrate/20150218032907_create_locations.rb
+++ db/migrate/20150218032907_create_locations.rb
@@ -3,7 +3,7 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.string :name
       t.string :address
-      
+
       t.timestamps null: false
     end
   end
