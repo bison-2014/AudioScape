26fe39e02ccd0265ac2c465fb0c562a84324c65e
s12dsow <s12.dsow@gmail.com>
2015-02-17 21:57:59 -0600

    added location migration

--- db/migrate/20150218032907_create_locations.rb
+++ db/migrate/20150218032907_create_locations.rb
@@ -3,6 +3,7 @@ class CreateLocations < ActiveRecord::Migration
     create_table :locations do |t|
       t.string :name
       t.string :address
+      t.name :title
 
       t.timestamps null: false
     end
